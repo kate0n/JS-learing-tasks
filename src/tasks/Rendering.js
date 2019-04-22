968d90d8a1349dfc1b96bfd22eff269336862d6a
Ekaterina <mikhailina.ekaterina@gmail.com>
2019-04-22 13:10:33 +0300

    на рендер

---
+++ src/tasks/Rendering.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+// import './App.css';
+
+const text1 = <p>текст1</p>;
+const text2 = <p>текст2</p>;
+class Rendering extends Component {
+    render() {
+        return (
+            <div>
+                {text1}
+                <p>!!!</p>
+                {text2}
+            </div>
+        );
+    }
+}
+
+export default Rendering;
