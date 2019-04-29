5de10a06992b2b90a5d7cfd6e8ddbafdc72be1a4
Ekaterina <mikhailina.ekaterina@gmail.com>
2019-04-29 19:06:55 +0300

    add OnSubmit.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React, {Component, Fragment} from 'react';
-import Rendering from './tasks/Rendering';
-import Attributes from './tasks/Attributes';
-import OnCss from './tasks/OnCss';
-import Condition from './tasks/Condition';
-// import Cycles from './tasks/Cycles';
-import Methods from './tasks/Methods';
-import States from './tasks/States';
+
+
 import Form from './tasks/Form';
-import CyclesV1 from './tasks/CyclesV1';
-import CyclesV2 from './tasks/CyclesV2';
-import CyclesV3_1 from './tasks/CyclesV3_1';
-import CyclesV3_2 from "./tasks/CyclesV3_2";
-import CyclesV3_3 from "./tasks/CyclesV3_3";
-import CyclesV3_4 from "./tasks/CyclesV3_4";
 import Inputs from "./tasks/Inputs";
 import Input2 from "./tasks/Input2";
 import Input3 from "./tasks/Input3";
@@ -24,26 +13,14 @@ import Map from "./tasks/Arrays/Map";
 import Filter from "./tasks/Arrays/Filter";
 import Find from "./tasks/Arrays/Find";
 import ArrowFunc from "./tasks/ArrowFunc";
+import OnSubmit from "./tasks/OnSubmit";
 
 
 class App extends Component {
     render() {
         return (
             <Fragment>
-                <Rendering/>
-                <Attributes/>
-                <OnCss/>
-                <Condition/>
-                {/*<Cycles />*/}
-                <Methods/>
-                <States/>
                 <Form/>
-                <CyclesV1/>
-                <CyclesV2/>
-                <CyclesV3_1/>
-                <CyclesV3_2/>
-                <CyclesV3_3/>
-                <CyclesV3_4/>
                 <Inputs/>
                 <Input2/>
                 <Input3/>
@@ -57,6 +34,7 @@ class App extends Component {
                 <Find/>
 
                 <ArrowFunc/>
+                <OnSubmit/>
             </Fragment>
         );
     }
---
+++ src/tasks/OnSubmit.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+
+export class OnSubmit extends Component {
+    state = {
+        textValue: "",
+        sum: "",
+        fullName: ""
+    }
+
+    getText = React.createRef();
+    getNum1 = React.createRef();
+    getNum2 = React.createRef();
+    lastname = React.createRef();
+    name = React.createRef();
+    surname = React.createRef();
+
+    handleClick1 = () => {
+        const textValue = this.getText.current.value;
+        this.setState({textValue: textValue});
+    }
+
+    handleSum = () => {
+        const num1 = parseInt(this.getNum1.current.value);
+        const num2 = parseInt(this.getNum2.current.value);
+        this.setState({sum: num1 + num2});
+    }
+
+    handleFullName = () => {
+        const lastname = this.lastname.current.value;
+        const name = this.name.current.value;
+        const surname = this.surname.current.value;
+        this.setState({fullName: `${lastname} ${name} ${surname} `});
+    }
+
+    render() {
+        return (
+            <div>
+                {/*1 пункт*/}
+                <input
+                    ref={this.getText}
+                    placeholder={"ввывод в абзац"} />
+                <button onClick={this.handleClick1}> Submit </button>
+                <p> {this.state.textValue} </p>
+
+                {/*2 пункт*/}
+                <input
+                    ref={this.getNum1}
+                    placeholder={"1 число"} />
+                <input
+                    ref={this.getNum2}
+                    placeholder={"2 число"} />
+                <button onClick={this.handleSum}> Get sum </button>
+                <p> {this.state.sum} </p>
+
+                {/*3 пункт*/}
+                <input
+                    ref={this.lastname}
+                    placeholder={"фамилия"} />
+                <input
+                    ref={this.name}
+                    placeholder={"имя"} />
+                <input
+                    ref={this.surname}
+                    placeholder={"отчество"} />
+                <button onClick={this.handleFullName}> Submit </button>
+                <p> {this.state.fullName} </p>
+
+            </div>
+        )
+    }
+};
+
+export default OnSubmit;
