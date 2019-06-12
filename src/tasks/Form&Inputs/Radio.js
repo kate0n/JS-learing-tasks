d9749cf9406c43e88d9cfd2e044409eb25ab8ea0
Ekaterina <mikhailina.ekaterina@gmail.com>
2019-06-12 19:32:06 +0300

    fix Radio.js

--- src/tasks/Form&Inputs/Radio.js
+++ src/tasks/Form&Inputs/Radio.js
@@ -5,55 +5,37 @@ class Radio extends Component {
         radio: ""
     };
 
-    getRadio1 = React.createRef();
-    getRadio2 = React.createRef();
-    getRadio3 = React.createRef();
-
-
-    getRadioValue = () => {
-        const radio1 = this.getRadio1.current;
-        const radio2 = this.getRadio2.current;
-        const radio3 = this.getRadio3.current;
-        let radio = "";
-        if (radio1.checked) {
-            radio = radio1.value;
-        } else if (radio2.checked) {
-            radio = radio2.value;
-        } else if (radio3.checked) {
-            radio = radio3.value;
-        }
-        this.setState({radio: radio})
+    handleRadio = event => {
+        this.setState({ radio: event.target.value });
     };
 
     render() {
 
         return (
-            <form onClick={this.getRadioValue}>
+            <form>
                 <input
                     type={"radio"}
-                    id={"1"}
+                    id={"radio1"}
                     value={"1"}
                     name={"numbers"}
-                    ref={this.getRadio1}
-                />
-                <label htmlFor={"1"}> 1 </label>
-                <input
-                    type={"radio"}
-                    id={"2"}
-                    value={"2"}
-                    name={"numbers"}
-                    ref={this.getRadio2}
-                />
-                <label htmlFor={"1"}> 2 </label>
+                    onChange={this.handleRadio}/>
+                <label htmlFor={"radio1"}> 1 </label>
 
                 <input
                     type={"radio"}
-                    id={"3"}
+                    id={"radio12"}
+                    value={"2"}
+                    name={"numbers"}
+                    onChange={this.handleRadio}/>
+                <label htmlFor={"radio2"}> 2 </label>
+
+                <input
+                    type={"radio"}
+                    id={"radio13"}
                     value={"3"}
                     name={"numbers"}
-                    ref={this.getRadio3}
-                />
-                <label htmlFor={"1"}> 3 </label>
+                    onChange={this.handleRadio}/>
+                <label htmlFor={"radio3"}> 3 </label>
 
                 <div> {this.state.radio} </div>
 
